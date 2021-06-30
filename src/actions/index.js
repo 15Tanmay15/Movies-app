02fbe1157889a8c5e23798d86622e1b4ca8705c8
Tanmay <tanmaygpro@gmail.com>
2021-06-30 15:16:47 +0530

    changing state to add favourites

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,18 @@
 //     type: 'ADD_MOVIES',
         // movies: [m1, m2, m3]
 
+
 // }
 // {
 //     type: 'DECREASE_COUNT'
 // }
+
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+// action creators
+export function addMovies (movies) {
+        return {
+                type: ADD_MOVIES,
+                movies
+        }
+}
